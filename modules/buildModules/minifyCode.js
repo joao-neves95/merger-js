1b05ea0bd5f28c80dcede27c2af36ceeeb1f2b9e
João Neves <joao95neves@gmail.com>
2019-05-19 13:22:37 +0100

    Beautified the code; Added JSDoc comment.

--- modules/buildModules/minifyCode.js
+++ modules/buildModules/minifyCode.js
@@ -10,24 +10,30 @@
 const uglify = require('uglify-es');
 const notify = require('../notifications').notif;
 
-module.exports = (allCode, callback) => {
-  if (global.config.uglify) {
-    let minifiedCode = uglify.minify(allCode, global.minifyOptions);
-    if (minifiedCode.error) {
+/**
+ * 
+ * @param { string } allCode Code input.
+ * 
+ * @param { Function } callback (string)
+ */
+module.exports = ( allCode, callback ) => {
+  if ( global.config.uglify ) {
+    let minifiedCode = uglify.minify( allCode, global.minifyOptions );
+    if ( minifiedCode.error ) {
       let err = minifiedCode.error;
-      notify(`Build error on the file ${err.filename}, line ${err.line}, col ${err.col}.`, err.message);
-      return console.error(' Error:\n ', err);
+      notify( `Build error on the file ${err.filename}, line ${err.line}, col ${err.col}.`, err.message );
+      return console.error( ' Error:\n ', err );
     }
-    if (global.minifyOptions.warnings && minifiedCode.warnings)
-      console.warn('\n Warnings: \n ', minifiedCode.warnings);
+    if ( global.minifyOptions.warnings && minifiedCode.warnings )
+      console.warn( '\n Warnings: \n ', minifiedCode.warnings );
 
-    return callback(minifiedCode.code);
+    return callback( minifiedCode.code );
   } else {
     let nonMinifiedCode = '';
-    for (let fileName in allCode) {
+    for ( let fileName in allCode ) {
       nonMinifiedCode += allCode[fileName];
     }
 
-    return callback(nonMinifiedCode);
+    return callback( nonMinifiedCode );
   }
-}
+};
