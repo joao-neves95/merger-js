4a1329d2f5e0f215e280d7ca2c096a5c8738fd26
João Neves <joao95neves@gmail.com>
2019-05-21 22:22:23 +0100

    Update utils_spec.js to avoid code duplication

--- tests/spec/utils_spec.js
+++ tests/spec/utils_spec.js
@@ -1,42 +1,37 @@
-﻿const {
-  removeImportFromInput,
-  removeDirTokenFromImport,
-  removeGithubTokenFromImport,
-  readDir
-} = require( '../../modules/utils' );
+﻿const Utils = require( '../../modules/utils' );
 const TokenType = require( '../../enums/tokenType' );
+const MockImports = require( '../data/mockImports' );
 
-const mockImport_DIR = `// ${TokenType.token_importPath}${TokenType.token_DIR} 'kdsf/fdjslkg/dfz'`;
-const mockImport_dir = `// ${TokenType.token_importPath}${TokenType.token_dir} 'kdsf/fdjslkg/dfz'`;
-const mockImport_DIRECTORY = `// ${TokenType.token_importPath}${TokenType.token_DIRECTORY} 'kdsf/fdjslkg/dfz'`;
-const mockImport_directory = `// ${TokenType.token_importPath}${TokenType.token_directory} 'kdsf/fdjslkg/dfz'`;
-
-const mockImport_github_DIR = `// ${TokenType.token_importUrl_simbol}${TokenType.token_github}${TokenType.token_DIR} 'https://kdsf/fdjslkg/dfz'`;
-const mockImport_GH_dir = `// ${TokenType.token_importUrl}${TokenType.token_GH}${TokenType.token_dir} 'https://kdsf/fdjslkg/dfz'`;
+const RANDOM_PATH_1 = 'kdsf/fdjslkg/dfz';
+const RANDOM_LINK_1 = `https://${RANDOM_PATH_1}`;
 
 describe( 'Utils', () => {
 
+  it( 'Should throw Error (static class)', () => {
+    expect( () => { new Utils(); } ).toThrowError( null );
+  } );
+
   it( 'Should remove all "import" tokens types from the import input.', () => {
-    expect( removeImportFromInput( mockImport_DIR ) ).not.toContain( TokenType.token_importPath );
-    expect( removeImportFromInput( mockImport_github_DIR ) ).not.toContain( TokenType.token_importUrl_simbol );
-    expect( removeImportFromInput( mockImport_GH_dir ) ).not.toContain( TokenType.token_importUrl );
+    expect( Utils.removeImportFromInput( MockImports.relative_DIR( RANDOM_PATH_1 ) ) ).not.toContain( TokenType.token_importPath );
+    expect( Utils.removeImportFromInput( MockImports.github_DIR( RANDOM_LINK_1 ) ) ).not.toContain( TokenType.token_importUrl_simbol );
+    expect( Utils.removeImportFromInput( MockImports.GH_dir( RANDOM_LINK_1 ) ) ).not.toContain( TokenType.token_importUrl );
   } );
 
   it( 'Should remove all GITHUB tokens from the import input.', () => {
-    expect( removeGithubTokenFromImport( mockImport_github_DIR ) ).not.toContain( TokenType.token_github );
-    expect( removeGithubTokenFromImport( mockImport_GH_dir ) ).not.toContain( TokenType.token_GH );
+    expect( Utils.removeGithubTokenFromImport( MockImports.github_DIR( RANDOM_LINK_1 ) ) ).not.toContain( TokenType.token_github );
+    expect( Utils.removeGithubTokenFromImport( MockImports.GH_dir( RANDOM_LINK_1 ) ) ).not.toContain( TokenType.token_GH );
   } );
 
   it( 'Should remove all DIRECTORY tokens from the import input.', () => {
-    expect( removeDirTokenFromImport( mockImport_dir ) ).not.toContain( TokenType.token_dir );
-    expect( removeDirTokenFromImport( mockImport_DIR ) ).not.toContain( TokenType.token_DIR );
-    expect( removeDirTokenFromImport( mockImport_github_DIR ) ).not.toContain( TokenType.token_DIR );
-    expect( removeDirTokenFromImport( mockImport_DIRECTORY ) ).not.toContain( TokenType.token_DIRECTORY );
-    expect( removeDirTokenFromImport( mockImport_directory ) ).not.toContain( TokenType.token_directory );
+    expect( Utils.removeDirTokenFromImport( MockImports.relative_dir( RANDOM_PATH_1 )) ).not.toContain( TokenType.token_dir );
+    expect( Utils.removeDirTokenFromImport( MockImports.relative_DIR( RANDOM_PATH_1 ) ) ).not.toContain( TokenType.token_DIR );
+    expect( Utils.removeDirTokenFromImport( MockImports.github_DIR( RANDOM_LINK_1 ) ) ).not.toContain( TokenType.token_DIR );
+    expect( Utils.removeDirTokenFromImport( MockImports.relative_DIRECTORY( RANDOM_PATH_1 ) ) ).not.toContain( TokenType.token_DIRECTORY );
+    expect( Utils.removeDirTokenFromImport( MockImports.relative_directory( RANDOM_PATH_1 ) ) ).not.toContain( TokenType.token_directory );
   } );
 
   it( 'Should read an entire directory.', async () => {
-    const dirPathsArr = await readDir( __dirname );
+    const dirPathsArr = await Utils.readDir( __dirname );
     expect( dirPathsArr ).toBeDefined();
     expect( dirPathsArr ).not.toBeNull();
     expect( dirPathsArr ).toEqual( jasmine.arrayContaining( ['httpClient_spec.js', 'parseImports_spec.js', 'mergerCLI_specs'] ) );
