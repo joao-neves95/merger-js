de1ecad266e911b3653b4e650ef6b2ce55025e6d
João Neves <joao95neves@gmail.com>
2019-05-21 22:17:09 +0100

    Added the ParseImports spec and the first test for relative file paths

--- tests/spec/parseImports_spec.js
+++ tests/spec/parseImports_spec.js
@@ -1,1 +1,13 @@
-﻿
+﻿const path = require( 'path' );
+const parseImports = require( '../../modules/buildModules/parseImports' );
+
+describe( 'ParseImports', () => {
+
+  it( 'Should parse relative file path imports.', () => {
+    parseImports( path.join( __dirname, path.normalize( '../data/headerFiles/relativeFilePaths.header.js' ) ), ( buildOrder ) => {
+
+      expect( buildOrder ).toEqual( jasmine.arrayContaining( ['relativeFilePaths.header.js', '../mockImports.js', '../../tests.js', '../../spec/utils_spec.js'] ) );
+    } );
+  } );
+
+} );
