0ea1b5f426c7b06fafde4a2c06ffb8b0464a4930
joao-neves95 <joao95neves@gmail.com>
2019-10-15 17:44:10 +0100

    Added the parsed line model

---
+++ models/parsedLineModel.js
@@ -0,0 +1,39 @@
+﻿const ImportType = require( '../enums/importType' );
+
+class ParsedLine {
+
+  constructor() {
+    /**
+     * @type { boolean }
+     */
+    this.isComment = null;
+
+    /**
+     * @type { boolean }
+     */
+    this.isDir = false;
+
+    /** ImportType enum
+     * @type { number } ImportType enum
+     */
+    this.importType = ImportType.Unknown;
+
+    /**
+     * @type { boolean }
+     */
+    this.forceInstall = false;
+
+    /**
+     * @type { string | null }
+     */
+    this.path = null;
+
+    /**
+     * @type { string | null }
+     */
+    this.branchName = null;
+  }
+
+}
+
+module.exports = ParsedLine;
