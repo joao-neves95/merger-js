fd76b28945832bb74592f1f5c8cbc614fadbbbb0
João Neves <joao95neves@gmail.com>
2020-06-11 15:44:28 +0100

    [modules/mergerCLI.js]: Added the "list" key to the `merger set` command

--- modules/mergerCLI.js
+++ modules/mergerCLI.js
@@ -89,7 +89,7 @@ module.exports = ( Callback ) => {
       } else if ( cmd.false ) {
         value = false;
 
-      } else if ( Key === 'HELP' || cmd.list || cmd.help ) {
+      } else if ( Key === 'HELP' || Key === 'LIST' || cmd.list || cmd.help ) {
         console.info( '\n Set MergerJS configuration.\n' );
         console.info( ' Configuration keys:' );
         console.info( ' -------------------' );
